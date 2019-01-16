e0bb7f71e860401beb74dec581cbc89ec691eaf2
loloh Mugisha <lolohmugisha@gmail.com>
2019-01-16 11:40:23 +0200

    add values

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown1").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
@@ -17,3 +19,41 @@ function myFunction() {
       }
     }
   }
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+  document.getElementById("myDropdown").classList.remove("show");
+  document.getElementById("myDropdown2").classList.remove("show");
+}
+1
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.vendors')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+  document.getElementById("myDropdown1").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dresses')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
